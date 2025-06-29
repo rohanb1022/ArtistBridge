59d743b60bc6fd110300882daba3b3cef273eb5d
Rohan Bhangale <rohanbhangale25@gmail.com>
2025-06-29 12:04:55 +0530

    login page added for both artist and org

--- app/auth/artist/login/page.tsx
+++ app/auth/artist/login/page.tsx
@@ -1,6 +1,8 @@
-"use client"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardAction,
@@ -9,60 +11,96 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Link from "next/link"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import api from "@/lib/axios";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation"
+ 
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const router = useRouter();
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-const CardDemo = () => {
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const res = await api.post("/auth/artist/login", formData);
+      console.log("login successs", res.data);
+      router.push("/")
+    } catch (error: any) {
+      const errorMessage = error.response?.data || "Something went wrong";
+      setError(errorMessage);
+    }
+  };
 
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-        <CardAction>
-         <Link href={"/auth/organizer/signup"} >
-          <Button variant="link">signup</Button>
-          </Link>
-        </CardAction>
-      </CardHeader>
-      <CardContent>
-        <form>
+    <form onSubmit={handleSubmit}>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+          <CardAction>
+            <Link href={"/auth/organizer/signup"}>
+              <Button variant="link">signup</Button>
+            </Link>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
                 type="email"
+                name="email"
                 placeholder="m@example.com"
                 required
+                onChange={handleChange}
               />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
                 <Label htmlFor="password">Password</Label>
               </div>
-              <Input id="password" type="password" required />
+              <Input
+                id="password"
+                type="password"
+                required
+                name="password"
+                onChange={handleChange}
+              />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Link href={"/auth/organizer/login"} >
-        <Button variant="outline" className="w-[360px]">
-          login as Organizer
-        </Button>
-        </Link>
-      </CardFooter>
-    </Card>
-  )
-}
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Link href={"/auth/organizer/login"}>
+            <Button variant="outline" className="w-[360px]">
+              login as Organizer
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+};
 
-export default CardDemo
+export default Login;

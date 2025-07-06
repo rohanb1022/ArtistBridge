7cb972f76181ccd108712bc97dfdc2ff9f95e9f4
Rohan Bhangale <rohanbhangale25@gmail.com>
2025-07-06 08:37:01 +0530

    Change fetching params method in artsit-profile page

--- app/api/organizer/artist-profile/[artistId]/route.ts
+++ app/api/organizer/artist-profile/[artistId]/route.ts
@@ -1,9 +1,13 @@
 import prisma from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest, context : { params: { artistId: string } }) {
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const artistId = url.pathname.split("/").pop(); 
+
+
   try {
-    const id = parseInt(context.params.artistId); // safely parse it
+    const id = parseInt(artistId ?? ""); // safely parse it
     const artist = await prisma.artist.findUnique({
       where: { id },
     });
